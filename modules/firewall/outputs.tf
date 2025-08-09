95971bbd8e1e0dac0806ea7eec2053d449a23d04
Muralee2 <muralimvkaws1@gmail.com>
2025-08-10 00:22:48 +0530

    Update outputs.tf

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -5,3 +5,7 @@ output "allow_gke_master_rule" {
 output "allow_internal_rule" {
   value = google_compute_firewall.allow_internal.name
 }
+
+output "network_self_link" {
+  value = google_compute_network.my_network.self_link
+}
