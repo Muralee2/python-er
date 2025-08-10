2e6fb6103888ebd5d7d6a2d9098e3cd37f036d04
Muralee2 <muralimvkaws1@gmail.com>
2025-08-10 12:42:35 +0530

    Update versions.tf

--- modules/network/versions.tf
+++ modules/network/versions.tf
@@ -1,13 +1,3 @@
 terraform {
-  required_version = ">= 1.3.0"
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-variable "project_id" {
-  type        = string
-  description = "The Google Cloud project ID"
+  required_version = ">= 1.6.0"
 }
