dfa95f6d7337f1b6f86a1f618f34f454ef854c8f
Muralee2 <muralimvkaws1@gmail.com>
2025-08-10 12:44:49 +0530

    Update outputs.tf

--- modules/gke-cluster/outputs.tf
+++ modules/gke-cluster/outputs.tf
@@ -1,19 +1,7 @@
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
 output "cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
+  value = google_container_cluster.gke.endpoint
 }
 
-output "cluster_location" {
-  value = google_container_cluster.primary.location
-}
-
-output "node_pool_name" {
-  value = google_container_node_pool.primary_nodes.name
-}
-
-output "node_service_account" {
-  value = google_service_account.gke_sa.email
+output "cluster_name" {
+  value = google_container_cluster.gke.name
 }
