fe9aafffac9d5936c45eedb00d7408a4db9f06c3
Muralee2 <muralimvkaws1@gmail.com>
2025-08-10 12:43:31 +0530

    Update variables.tf

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -1,14 +1,4 @@
-variable "network_self_link" {
-  description = "Self link of the network"
-  type        = string
-}
-
-variable "network_name" {
-  description = "Name of the network"
-  type        = string
-}
-
-variable "project_id" {
-  description = "GCP Project ID"
-  type        = string
-}
+variable "project_id" { type = string }
+variable "region" { type = string }
+variable "network_name" { type = string }
+variable "network_self_link" { type = string }
