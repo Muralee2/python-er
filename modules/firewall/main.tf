bd1a817120a3f3358196498d3feb2e72d97d8f04
Muralee2 <muralimvkaws1@gmail.com>
2025-08-10 12:43:13 +0530

    Update main.tf

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -1,47 +1,27 @@
-terraform {
-  required_version = ">= 1.3.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 2.0"
-    }
-  }
-}
-
-data "external" "firewall_rules" {
-  program = ["python3", "${path.module}/../../scripts/generate_firewall.py"]
-
-  query = {
-    network_name = var.network_name
-    project_id   = var.project_id
-  }
-}
-
-resource "google_compute_firewall" "allow_gke_master" {
-  name    = data.external.firewall_rules.result["allow_gke_master_name"]
-  network = var.network_self_link
-
-  allow {
-    protocol = "tcp"
-    ports    = ["443"]
-  }
-
-  source_ranges = data.external.firewall_rules.result["allow_gke_master_ranges"]
-  direction     = "EGRESS"
+provider "google" {
+  project = var.project_id
+  region  = var.region
 }
 
 resource "google_compute_firewall" "allow_internal" {
-  name    = data.external.firewall_rules.result["allow_internal_name"]
+  name    = "${var.network_name}-allow-internal"
   network = var.network_self_link
 
-  allow {
+  allows {
     protocol = "all"
   }
 
-  source_ranges = data.external.firewall_rules.result["allow_internal_ranges"]
-  direction     = "INGRESS"
+  source_ranges = ["10.0.0.0/8"]
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "${var.network_name}-allow-ssh"
+  network = var.network_self_link
+
+  allows {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
