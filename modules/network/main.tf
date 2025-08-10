3f1adc2b4b1f049197af2faa692a4b1ef0d6afb7
Muralee2 <muralimvkaws1@gmail.com>
2025-08-10 12:41:47 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,13 +1,15 @@
 terraform {
-  required_version = ">= 1.3.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
   }
+}
 
-  backend "gcs" {} # <-- Needed for Terragrunt remote_state
+provider "google" {
+  project = var.project_id
+  region  = var.region
 }
 
 resource "google_compute_network" "vpc" {
@@ -20,12 +22,5 @@ resource "google_compute_subnetwork" "subnet" {
   ip_cidr_range = var.subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc.id
-}
-
-output "network_self_link" {
-  value = google_compute_network.vpc.self_link
-}
-
-output "subnet_self_link" {
-  value = google_compute_subnetwork.subnet.self_link
+  private_ip_google_access = true
 }
