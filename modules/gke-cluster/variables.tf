588e85e0f248669a01e21a34c9277ab09881cc11
Muralee2 <muralimvkaws1@gmail.com>
2025-08-11 02:03:11 +0530

    Update variables.tf

--- modules/gke-cluster/variables.tf
+++ modules/gke-cluster/variables.tf
@@ -1,5 +1,4 @@
 variable "project_id" { type = string }
 variable "region" { type = string }
 variable "cluster_name" { type = string }
-variable "node_machine_type" { type = string }
-variable "node_count" { type = number }
+
