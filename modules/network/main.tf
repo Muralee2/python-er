e4995aa3d81b38277e7c1b152a376948c924ad18
Muralee2 <muralimvkaws1@gmail.com>
2025-08-11 00:49:39 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -4,10 +4,10 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = var.subnet_name
-  ip_cidr_range = var.subnet_cidr
-  region        = var.region
-  network       = google_compute_network.vpc.id
+  name                     = var.subnet_name
+  ip_cidr_range            = var.subnet_cidr
+  region                   = var.region
+  network                  = google_compute_network.vpc.name
   private_ip_google_access = true
 
   # Secondary ranges for GKE
