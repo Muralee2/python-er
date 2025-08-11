bf802fc78c8f32510e3c84158540c6e00158bff7
Muralee2 <muralimvkaws1@gmail.com>
2025-08-11 14:03:31 +0530

    Update outputs.tf

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -1,4 +1,3 @@
-output "firewall_rule_names" {
-  description = "Names of the created firewall rules."
-  value       = [for rule in google_compute_firewall.gke_firewall : rule.name]
+output "firewall_status" {
+  value = "Firewall rule applied for ${var.network}/${var.subnet}"
 }
