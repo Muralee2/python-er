2ba8fceac6ba1f07af32fe57aa30168cb3899a54
Muralee2 <muralimvkaws1@gmail.com>
2025-08-11 13:40:19 +0530

    Update variables.tf

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -1,4 +1,16 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "network_name" { type = string }
+variable "network_name" {
+  description = "Name of the network where firewall rules will be applied."
+  type        = string
+}
 
+variable "firewall_rules" {
+  description = "List of firewall rules."
+  type = list(object({
+    name          = string
+    direction     = string
+    protocol      = string
+    ports         = list(string)
+    source_ranges = list(string)
+    target_tags   = list(string)
+  }))
+}
